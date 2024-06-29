4853378183d433c3c409380ec4ccb8a22ce39650
jpphang <joyanna_phang1@my.vcccd.edu>
2024-06-29 06:01:17 -0700

    48

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ def main():
     begin = int(input("Enter the first number here:"))
     end = int(input("Enter the ending number here:"))
     
+    for number in range(begin , end + 1):
+        if number > 1:
+            for i in range (2 , number):
+                in number % 2 == 0:
+                    break
+    
     
 
     return plist
